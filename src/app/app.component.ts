a6f2df584ded569ff0dbc293af54c0c6f2bfd5ef
CamBBOSS <cam@bboss.biz>
2022-03-02 18:29:41 +0000

    4 files modified, 5 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,4 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Angular ' + VERSION.major;
-  name = 'Cam';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
+  bootstrap: [AppComponent],
+  providers: [],
 })
-export class AppModule { }
+export class AppModule {}
